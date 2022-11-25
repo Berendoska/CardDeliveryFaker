12dbc49900cfb61081272739751108ef5058d1ef
kate <k-b-v@yandex.ru>
2022-11-25 15:19:09 +0300

    new jar

--- src/test/java/ru/netology/BankFaker.java
+++ src/test/java/ru/netology/BankFaker.java
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 import static com.codeborne.selenide.Selenide.open;
+//
 
 
 class BankFaker {
